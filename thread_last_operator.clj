e26568ee9a4073f21c2485fb0286ba9e3fe250e1
Volkmar Carrillo <carrillo.ramklov@gmail.com>
2023-01-25 01:35:54 -0500

    Verificación de Claves del archivo invoice.edn y Solución del Problema 1 - Thread-Last Operator 💯✌️

--- thread_last_operator.clj
+++ thread_last_operator.clj
@@ -1,3 +1,18 @@
 (ns thread_last_operator)
 
 (def invoice (clojure.edn/read-string (slurp "invoice.edn")))
+
+(defn
+  valid_item? [{taxes :taxable/taxes
+                retentions :retentionable/retentions}]
+
+  (let [valid_retention_rate?  (->> retentions (some #(= (:retention/rate %) 1)))
+        valid_tax_rate? (->> taxes (some #(= (:tax/rate %) 19)))]
+    (if (and valid_retention_rate? valid_tax_rate?)
+      false
+      (or valid_retention_rate? valid_tax_rate?))))
+
+(defn init
+  [invoice]
+  (->> (get invoice :invoice/items)
+       (filter valid_item?)))
